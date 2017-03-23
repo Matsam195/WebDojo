25b6ac9b43fc2e8f0e5982a4d0cf72723dc8c22e
Kevin <kevinj@theodo.fr>
2017-03-23 16:51:41 +0100

    Tentative d'avoir une réponse du chatbot

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,14 +1,30 @@
 var express = require('express');
 var router = express.Router();
-var authenticate = require('../server/chatService');
+var chatService = require('../server/chatService');
 
 router.get('/', function(req, res, next) {
-  if (authenticate.authenticate(req)) {
+  if (chatService.authenticate(req)) {
     res.status(200).send(req.query['hub.challenge']);
   } else {
     res.sendStatus(403);
   }
-  res.send('ok');
 });
+
+router.post('/webhook', function (req, res) {
+var data = req.body;
+if (data.object === 'page') {
+  data.entry.forEach(function(entry) {
+    var pageID = entry.id;
+    var timeOfEvent = entry.time;
+    entry.messaging.foreach(function(event) {
+      if (event.message) {
+        chatService.receivedMessage(event);
+      } else {
+        console.log("Webhook received unknown event: ", event);
+      }
+    });
+  });
+  chatService.sendTextMessage(event.sender.id, event.message.text)
+}});
 
 module.exports = router;
