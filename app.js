7f0e360a2939d7d572f52283064d9686a4936c15
Kevin <kevinj@theodo.fr>
2017-03-23 15:53:43 +0100

    Renvoi d'un Forbidden en créant le webhook.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var homepage = require('./routes/index');
 
+var webhook = require('./routes/webhook');
 var app = express();
 
 // view engine setup
@@ -22,6 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', homepage);
+app.use('/webhook', webhook);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
